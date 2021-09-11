575077f8964c7e11bfcd05b8a13e7462f375e52d
blackaosdragon <ka_ivj@live.com>
2021-09-11 01:46:21 -0500

    Cambios minimos

--- src/componentes/contador.js
+++ src/componentes/contador.js
@@ -3,7 +3,9 @@ import React from "react"
 const numbers = new Array(100).fill()
 //console.log(numbers)
 class Contador extends React.Component {
+
     render() {
+
         /*
         const lista = numeros.map(element =>
             <div key={element.toString()}>
@@ -15,41 +17,26 @@ class Contador extends React.Component {
             switch (id % 5) {
                 case 0:
                     return (<div style={{ backgroundColor: "white" }}>
-                        {console.log(id + 1)}
                         {id + 1}
                     </div>)
                 case 1:
                     return (<div style={{ backgroundColor: "red" }}>
-                        {console.log(id + 1)}
                         {id + 1}
                     </div>)
                 case 2:
                     return (<div style={{ backgroundColor: "green" }}>
-                        {console.log(id + 1)}
                         {id + 1}
                     </div>)
                 case 3:
                     return (<div style={{ backgroundColor: "blue" }}>
-                        {console.log(id + 1)}
                         {id + 1}
                     </div>)
                 case 4:
                     return (<div style={{ backgroundColor: "black", color: "white" }}>
-                        {console.log(id + 1)}
                         {id + 1}
                     </div>)
-
-
             }
-
-            /*return (<div style={{ backgroundColor: "green" }}>
-                {console.log(id + 1)}
-                {id + 1}
-            </div>)*/
-        }
-
-        )
-
+        })
         return (
             <div>
                 {list}
